597c068d57e81970c3e1678892789c19c8617a51
Michał Kotowski <dev@kotowski.xyz>
2020-12-04 20:20:46 +0100

    Add vertical and horizontal scrolling

--- main.cpp
+++ main.cpp
@@ -32,6 +32,8 @@ typedef struct erow
 
 struct editorConfig {
 	int cx, cy;
+	int rowoff;
+	int coloff;
 	int screenrows;
 	int screencols;
 	int numrows;
@@ -311,12 +313,33 @@ void abFree(struct abuf *ab)
 	free(ab->b);
 }
 
+void editorScroll()
+{
+	if (E.cy < E.rowoff)
+	{
+		E.rowoff = E.cy;
+	}
+	if (E.cy >= E.rowoff + E.screenrows)
+	{
+		E.rowoff = E.cy - E.screenrows + 1;
+	}
+	if (E.cx < E.coloff)
+	{
+		E.coloff = E.cx;
+	}
+	if (E.cx >= E.coloff + E.screencols)
+	{
+		E.coloff = E.cx - E.screencols + 1;
+	}
+}
+
 void editorDrawRows(struct abuf *ab)
 {
 	int y;
 	for (y = 0; y < E.screenrows; y++)
 	{
-		if (y >= E.numrows)
+		int filerow = y + E.rowoff;
+		if (filerow >= E.numrows)
 		{
 			if (E.numrows == 0 && y == E.screenrows / 3)
 			{
@@ -344,12 +367,19 @@ void editorDrawRows(struct abuf *ab)
 		}
 		else
 		{
-			int len = E.row[y].size;
+			int len = E.row[filerow].size - E.coloff;
+			
+			if (len < 0)
+			{
+				len = 0;
+			}
+
 			if (len > E.screencols)
 			{
 				len = E.screencols;
 			}
-			abAppend(ab, E.row[y].chars, len);
+
+			abAppend(ab, &E.row[filerow].chars[E.coloff], len);
 		}
 
 		abAppend(ab, "\x1b[K", 3);
@@ -362,6 +392,8 @@ void editorDrawRows(struct abuf *ab)
 
 void editorRefreshScreen()
 {
+	editorScroll();
+
 	struct abuf ab = ABUF_INIT;
 
 	abAppend(&ab, "\x1b[?25l", 6);
@@ -370,7 +402,8 @@ void editorRefreshScreen()
 	editorDrawRows(&ab);
 
 	char buf[32];
-	snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+	snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1,
+		   	(E.cx - E.coloff) + 1);
 	abAppend(&ab, buf, strlen(buf));
 
 	abAppend(&ab, "\x1b[?25h", 6);
@@ -390,10 +423,7 @@ void editorMoveCursor(int key)
 			}
 			break;
 		case ARROW_RIGHT:
-			if (E.cx != E.screencols - 1)
-			{
-				E.cx++;
-			}
+			E.cx++;
 			break;
 		case ARROW_UP:
 			if (E.cy != 0)
@@ -402,7 +432,7 @@ void editorMoveCursor(int key)
 			}
 			break;
 		case ARROW_DOWN:
-			if (E.cy != E.screenrows - 1)
+			if (E.cy != E.numrows - 1)
 			{
 				E.cy++;
 			}
@@ -450,6 +480,8 @@ void initEditor()
 {
 	E.cx = 0;
 	E.cy = 0;
+	E.rowoff = 0;
+	E.coloff = 0;
 	E.numrows = 0;
 	E.row = NULL;
 
