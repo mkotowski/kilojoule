e2eda8fb8cb8d1dd0a21dc319dc8664f108829f5
Michał Kotowski <dev@kotowski.xyz>
2020-12-04 21:58:57 +0100

    Add proper tab rendering

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
 #include <unistd.h>
 
 #define KILOJOULE_VERSION "0.1.0"
+#define KILOJOULE_TAB_STOP 4
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 enum editorKey
@@ -27,7 +28,9 @@ enum editorKey
 typedef struct erow
 {
 	int size;
+	int rsize;
 	char* chars;
+	char* render;
 } erow;
 
 struct editorConfig {
@@ -247,6 +250,41 @@ int getWindowSize(int* rows, int* cols)
 	}
 }
 
+void editorUpdateRow(erow* row)
+{
+	int tabs = 0;
+
+	for (int j = 0; j < row->size; j++)
+	{
+		if (row->chars[j] == '\t')
+		{
+			tabs++;
+		}
+	}
+
+	free(row->render);
+	row->render = (char*)malloc(row->size + tabs*(KILOJOULE_TAB_STOP-1) + 1);
+
+	int idx = 0;
+	for (int j = 0; j < row->size; j++)
+	{
+		if (row->chars[j] == '\t')
+		{
+			row->render[idx++] = ' ';
+			while (idx % KILOJOULE_TAB_STOP != 0)
+			{
+				row->render[idx++] = ' ';
+			}
+		}
+		else
+		{
+			row->render[idx++] = row->chars[j];
+		}
+	}
+	row->render[idx] = '\0';
+	row->rsize = idx;
+}
+
 void editorAppendRow(char* s, size_t len)
 {
 	E.row = (erow*)realloc(E.row, sizeof(erow) * (E.numrows+1));
@@ -256,6 +294,11 @@ void editorAppendRow(char* s, size_t len)
 	E.row[at].chars = (char*)malloc(len + 1);
 	memcpy(E.row[at].chars, s, len);
 	E.row[at].chars[len] = '\0';
+
+	E.row[at].rsize = 0;
+	E.row[at].render = NULL;
+	editorUpdateRow(&E.row[at]);
+
 	E.numrows++;
 }
 
@@ -367,7 +410,7 @@ void editorDrawRows(struct abuf *ab)
 		}
 		else
 		{
-			int len = E.row[filerow].size - E.coloff;
+			int len = E.row[filerow].rsize - E.coloff;
 			
 			if (len < 0)
 			{
@@ -379,7 +422,7 @@ void editorDrawRows(struct abuf *ab)
 				len = E.screencols;
 			}
 
-			abAppend(ab, &E.row[filerow].chars[E.coloff], len);
+			abAppend(ab, &E.row[filerow].render[E.coloff], len);
 		}
 
 		abAppend(ab, "\x1b[K", 3);
